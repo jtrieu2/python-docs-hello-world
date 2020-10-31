6fa5a1a088e0a954004bbbf45a378874a19ef671
Jenny Trieu <jtrieu@leaporbit.com>
2020-10-31 11:24:39 -0700

    migrations staged

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:20loves12@localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo: {self.id} name: {self.description}>'
+
+db.create_all()
+
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+   return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.get_json()['description']
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return jsonify ({
+      'description': todo.description
+  })
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- databasedemo.py
+++ databasedemo.py
@@ -1,16 +1,15 @@
 import psycopg2
-connection = psycopg2.connect('dbname=jennydemo')
+connection = psycopg2.connect('dbname=postgres user=postgres password=20loves12')
 cursor = connection.cursor()
 
 cursor.execute('''
-    CREATE TABLE table2 (
+    CREATE TABLE table3 (
         id INTEGER PRIMARY KEY,
         completed BOOLEAN NOT NULL DEFAULT False
     );
 ''')
 
-cursor.execute('INSERT INTO table2 (id, completed) VALUES (1,true);')
-git
+cursor.execute('INSERT INTO table3 (id, completed) VALUES (1,true);')
 connection.commit()
 
 connection.close()
